3951ebe88909d7fb90f02202fa6f90de95b4c978
latioms <latioms@gmail.com>
2023-03-19 16:47:07 +0100

    Dockerizing flask app

--- server/app.py
+++ server/app.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-      # return render_template('index.html')
-      return 'Fuck the World'
+      return render_template('index.html')
 
 # run server
 if __name__ == '__main__':
